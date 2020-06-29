e0607cd0a51f1e554443617ebca9df5fcf611744
Matt Zhang <set.stun@gmail.com>
2020-06-29 11:16:42 -0500

    Added soccer playing code

---
+++ Misc/soccer.py
@@ -0,0 +1,122 @@
+import random
+import math
+
+
+class Player:
+    def __init__(self, name):
+        self.pass_probs = []
+        self.team = None
+        self.name = name
+
+    def pass_ball(self):
+        pass_prob = random.random()
+        cumulative_prob = 0
+        for prob, player in self.pass_probs:
+            cumulative_prob += prob
+            if pass_prob <= cumulative_prob:
+                if player is None:
+                    print(">", self.name, "has scored!")
+                else:
+                    if self.team == player.team:
+                        print(">", self.name, "has passed to", player.name)
+                    else:
+                        print(">", self.name, "has been intercepted by", player.name)
+                return player
+        assert(1 == 2)
+
+
+class Team:
+    def __init__(self, name, roster):
+        self.name = name
+        self.roster = roster
+        for player in roster:
+            player.team = self
+        self.score = 0
+        self.other_defender = None
+
+    def score_goal(self):
+        self.score += 1
+
+    def has_won(self):
+        has_won = self.score >= 10
+        if has_won:
+            print(self.name, "have won!")
+        return has_won
+
+
+if __name__ == "__main__":
+    # initialize players
+    defender_A = Player("Antonio")
+    offender_A = Player("Armando")
+    goalie_A = Player("Arsenio")
+    defender_B = Player("Broca")
+    offender_B = Player("Brando")
+    goalie_B = Player("Benicio")
+
+    # set passing probs
+    defender_A.pass_probs = [(0.8, offender_A), (0.2, offender_B)]
+    offender_A.pass_probs = [(0.5, None), (0.5, goalie_B)]
+    goalie_A.pass_probs = [(0.8, defender_A), (0.2, offender_B)]
+    defender_B.pass_probs = [(0.75, offender_B), (0.25, offender_A)]
+    offender_B.pass_probs = [(0.45, None), (0.55, goalie_A)]
+    goalie_B.pass_probs = [(0.75, defender_B), (0.25, offender_A)]
+    team_A = Team("Flying Gorillas", [defender_A, offender_A, goalie_A])
+    team_B = Team("One Eyed Snakes", [defender_B, offender_B, goalie_B])
+    team_A.other_defender = defender_B
+    team_B.other_defender = defender_A
+
+    # stupid stuff to say
+    def commentate(player):
+        sayings = [
+            "I haven't seen play this intense since they banned dogs from playing soccer!",
+            "This game really makes me glad I have eyeballs!",
+            player.team.name + " really making a big comeback from their big doping scandal.",
+            player.team.name + " putting in the effort despite having had twelve concussions this season.",
+            "Back when my county existed, presidential elections were decided via soccer.",
+            "Soccer has really improved since they made the ball spherical instead of rectangular.",
+            "Enjoy this game while it lasts! Economists predict all soccer will be played by robots within the next five years.",
+            player.name + " using his secret kicking move that has defied physicists for decades.",
+            "Don't forget to buy your " + player.team.name + " merch after the game!",
+            "What a shameful display. Truly awful soccer playing.",
+            "Soccer hasn't been the same since they outlawed strategic groin kicks.",
+            "This is the kind of game that ended the Cold War!",
+            "Are there games other than soccer? I don't know and I don't want to know.",
+            "What are they doing? Oh wait, they're kicking the ball.",
+            "This game doesn't have nearly as many fatalities as it used to!",
+            "Can you believe " + player.name + " isn't even getting paid for this?",
+            player.team.name + " have a lot riding on this game. If they lose, they'll all have their passports revoked.",
+            "Oh, there goes a streaker on the field!",
+            "My wife left me because I loved soccer too much, but it was worth it!",
+            "What would people do without soccer? Probably farm or something.",
+            "The referee appears to be taking a nap, but he'll be up soon!",
+            "FIFA is considering adding microtransactions to soccer in real life. Exciting!",
+            "Soccer is short for 'Association Football'.",
+            player.name + " is doing pretty good for his first day on the job!",
+            "Fun fact: none of the players present here today actually know all the rules of soccer!",
+            "I wonder what's for lunch. Hopefully not sloppy joes again.",
+            "I also do announcements for children's birthday parties! Come talk to me after the game!",
+            "Looks like they forgot to inflate the ball before the game.",
+            "I hope they don't fire me for living in the announcer's booth.",
+            "What is soccer? Scientists are *this close* to finding out.",
+            player.name + " playing very well for a man with only one leg.",
+        ]
+        print(sayings[math.floor(random.random()*len(sayings))])
+
+    # play game
+    print("Game start!")
+    print()
+    player_with_ball = offender_A
+    while True:
+        current_team = player_with_ball.team
+        next_player = player_with_ball.pass_ball()
+        if random.random() < 0.25:
+            commentate(player_with_ball)
+        player_with_ball = next_player
+        if player_with_ball is None:
+            current_team.score_goal()
+            print("The score is now:", team_A.name + ":", str(team_A.score) + ",", team_B.name + ":", team_B.score)
+            print()
+            if current_team.has_won():
+                break
+            player_with_ball = current_team.other_defender
+            print(">", player_with_ball.name, "now has the ball")
