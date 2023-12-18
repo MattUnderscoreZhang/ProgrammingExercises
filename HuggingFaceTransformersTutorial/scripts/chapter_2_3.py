d8127b0d240f648d376d9b1e45b226f02e94b6e3
Matt Zhang <set.stun@gmail.com>
2023-12-17 20:22:46 -0500

    Chapter 2_3

---
+++ HuggingFaceTransformersTutorial/scripts/chapter_2_3.py
@@ -0,0 +1,22 @@
+from transformers import BertConfig, BertModel, BertTokenizer
+
+
+def randomly_initialize_model():
+    config = BertConfig()
+    model = BertModel(config)
+    return model
+
+
+def load_model():
+    model = BertModel.from_pretrained("bert-base-cased")
+    return model
+
+
+def save_model(model: BertModel):
+    model.save_pretrained("my_local_bert")
+
+
+if __name__ == "__main__":
+    # model = randomly_initialize_model()
+    # model = load_model()
+    # save_model(model)
